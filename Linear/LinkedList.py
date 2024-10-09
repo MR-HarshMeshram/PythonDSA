0d518fe1f5158780fc8429671c308917a7b3f133
Harsh <harshmeshram@gmail.com>
2024-10-09 19:34:30 +0530

    crerate delete method

--- Linear/LinkedList.py
+++ Linear/LinkedList.py
@@ -30,11 +30,31 @@ class LinkedList:
         while( current_node is not None):
             print(f" {current_node.data} ->",end='')
             current_node=current_node.next
+        print()
+    def DeleteFront(self):
+        if self.head is None:
+            print(" linkedList is empty ")
+            return
+        self.head=self.head.next
+    def DeleteLast(self):
+        if self.head is None:
+            print(" list is empty ")
+            return
+        if self.head.next is None:
+            self.head=self.head.next
+        current_node=self.head
+        while(current_node.next.next is not None):
+            current_node=current_node.next
+        current_node.next=None
 
 LL=LinkedList()
 LL.AddFirst(1)
 LL.AddFirst(2)
 LL.AddFirst(3)
 LL.AddLast(100)
+LL.PrintLinkedList()
+LL.DeleteFront()
+LL.PrintLinkedList()
+LL.DeleteLast()
 LL.PrintLinkedList()
 
